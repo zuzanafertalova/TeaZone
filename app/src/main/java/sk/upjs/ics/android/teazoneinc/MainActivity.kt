1928af3b6f164432d63810c9b9ca91729b8eabf0
Tomas Berezny <tberezny552@gmail.com>
2019-11-05 21:04:30 +0100

    Pridane nejake skarede farby

--- app/src/main/java/sk/upjs/ics/android/teazoneinc/MainActivity.kt
+++ app/src/main/java/sk/upjs/ics/android/teazoneinc/MainActivity.kt
@@ -2,11 +2,15 @@ package sk.upjs.ics.android.teazoneinc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_login.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
