8cca9b6e3ac5df24361a1efda7556c231b364069
Tomas Berezny <tberezny552@gmail.com>
2019-11-09 18:18:59 +0100

    Pridany HomeScreen Activita A taktiez uz ide sa zaregistrovat a prihlasit. Po prihlaseni ta hodi na homescreen

--- app/src/main/java/sk/upjs/ics/android/teazoneinc/LoginActivity.kt
+++ app/src/main/java/sk/upjs/ics/android/teazoneinc/LoginActivity.kt
@@ -3,14 +3,47 @@ package sk.upjs.ics.android.teazoneinc
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var auth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+
+        btnLogin.setOnClickListener(View.OnClickListener {
+
+            val email: String = tvLoginUsername.text.toString()
+            val password : String = pswdLoginPassword.text.toString()
+            if (email.isEmpty() || password.isEmpty()){
+                Toast.makeText(this,"Please fill up all fields",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) {
+                            task ->
+                        if (task.isSuccessful) {
+                            Log.w("SUCCESFULL LOGIN", "signInWithEmail:success")
+                            val user = auth.currentUser
+                            intent = Intent(this,HomeScreenActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Log.w("UNSUCCESFULL LOGIN", "signInWithEmail:failure", task.exception)
+                            Toast.makeText(baseContext, "daco nedobre moj", Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
+            }
+
+        })
 
         btnCreateAccount.setOnClickListener(View.OnClickListener {
             intent = Intent(this,SignupActivity::class.java)
--- app/src/main/java/sk/upjs/ics/android/teazoneinc/SignupActivity.kt
+++ app/src/main/java/sk/upjs/ics/android/teazoneinc/SignupActivity.kt
@@ -2,7 +2,10 @@ package sk.upjs.ics.android.teazoneinc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_signup.*
 
@@ -18,6 +21,39 @@ class SignupActivity : AppCompatActivity() {
         btnLoginToAccount.setOnClickListener(View.OnClickListener {
             finish()
         })
+
+        btnSignup.setOnClickListener(View.OnClickListener {
+            val email : String = tvSignupUsername.text.toString()
+            val password : String = pswdSignupPswrd.text.toString()
+            val confrimpassword: String = pswdSignupConfirmPswrd.text.toString()
+            if (email.isEmpty() || password.isEmpty() || confrimpassword.isEmpty()){
+                Toast.makeText(this,"Please fill up all fields",Toast.LENGTH_SHORT).show()
+            }
+            else {
+                if (password.equals(confrimpassword)){
+                    auth.createUserWithEmailAndPassword(email,password)
+                        .addOnCompleteListener(this){
+                                task ->
+                            if (task.isSuccessful) {
+                                val user = auth.currentUser
+                                Toast.makeText(this,"you have been Signed In. Please Log In",Toast.LENGTH_LONG).show()
+                                finish()
+                            }
+                            else {
+                                Toast.makeText(this, "An error occur, please try aggain later", Toast.LENGTH_SHORT).show()
+                                Log.w("POZRIME SA CO SA STALO", "createUserWithEmail:failure", task.exception)
+                            }
+
+                        }
+                }
+                else{
+                    Toast.makeText(this,"Your passwords does not match",Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        })
+
+
     }
 
 
