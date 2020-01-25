62a80bfb2533df5f77043012ad1108b7f30414aa
zuzana.fertalova <fertalova@spse-po.sk>
2020-01-25 18:31:33 +0100

    zmenená launcher ikona aj názov bez Inc

--- app/src/main/java/sk/upjs/ics/android/teazoneinc/HomeScreenActivity.kt
+++ app/src/main/java/sk/upjs/ics/android/teazoneinc/HomeScreenActivity.kt
@@ -2,20 +2,6 @@ package sk.upjs.ics.android.teazoneinc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-<<<<<<< Updated upstream
-import android.util.Log
-import sk.upjs.ics.android.teazoneinc.Firebase.User.DataUser
-=======
-import android.view.View
-import androidx.core.view.isVisible
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.EventListener
-import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.android.synthetic.main.activity_home_screen.*
-import kotlinx.android.synthetic.main.fragment_set_username2.*
-import sk.upjs.ics.android.teazoneinc.Firebase.User.dataFirma
-import sk.upjs.ics.android.teazoneinc.Firebase.User.dataUser
->>>>>>> Stashed changes
 import sk.upjs.ics.android.teazoneinc.Firebase.authentication.AuthAdapter
 import sk.upjs.ics.android.teazoneinc.Firebase.db.DbAdapter
 
@@ -29,33 +15,6 @@ class HomeScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
 
-<<<<<<< Updated upstream
-    }
-=======
-
-        fragmentSetUserame.view?.visibility = View.INVISIBLE
-        intent.extras?.let {
-            fragmentSetUserame.view?.visibility = View.VISIBLE
-        }
-
-        btnSetUsername.setOnClickListener(View.OnClickListener {
-            authAdapter.getUser()?.let {
-                dbAdapter.setUsersUsername(it,tvSetUsername.text.toString())
-            }
-            fragmentSetUserame.view?.visibility = View.GONE
-        })
 
     }
-
-    fun setUser(){
-
-    }
-
-    fun setFirmaUser(){
-
-    }
-
-
->>>>>>> Stashed changes
-
 }
