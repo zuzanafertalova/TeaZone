1fd4dc490a9ce864f6978443eee7cbfe6879ebd5
zuzana.fertalova <zuzana.fertalova@gmail.com>
2020-04-26 18:29:48 +0200

    treba srpavit funkciu

--- app/src/main/java/sk/upjs/ics/android/teazoneinc/HomeScreenFragments/ProfileFragments/FollowersProfileFragment.kt
+++ app/src/main/java/sk/upjs/ics/android/teazoneinc/HomeScreenFragments/ProfileFragments/FollowersProfileFragment.kt
@@ -1,6 +1,68 @@
 package sk.upjs.ics.android.teazoneinc.HomeScreenFragments.ProfileFragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_followers.*
+import sk.upjs.ics.android.teazoneinc.Adapters.Firebase.db.DbAdapterUser
+import sk.upjs.ics.android.teazoneinc.Adapters.FollowersAdapter
+import sk.upjs.ics.android.teazoneinc.R
+import java.util.*
+import kotlin.collections.ArrayList
 
 class FollowersProfileFragment : Fragment() {
+
+    val dbAdapterUser= DbAdapterUser()
+    private var followersFollowingIDList = ArrayList<String>()
+    private var followersFollowingList = ArrayList<String>()
+    internal var adapter: FollowersAdapter? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_followers, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpRecyclerView()
+//        getFollowersList(EventListener{ list, _->
+//            followersFollowingIDList=list!!
+//            dbAdapterUser.getFollowersFollowingUsername(list!!, EventListener{usernameList,_->
+//                followersFollowingList=usernameList!!
+//                addFollower()
+//            })
+//        })
+
+    }
+
+    private fun addFollower(){
+        setRecyclerData(followersFollowingList)
+    }
+
+    private fun setUpRecyclerView(){
+        rvFollowers.layoutManager = LinearLayoutManager(context)
+        adapter = FollowersAdapter()
+        rvFollowers.adapter = adapter
+    }
+
+//    fun getFollowersList(eventListener: EventListener<ArrayList<String>>){
+//        dbAdapterUser.getStatus(docID, EventListener{ status, _->
+//            if (status.equals("User")){
+//                dbAdapterUser.getFollowingList(docID, EventListener{ followingList, _->
+//                    eventListener.onEvent(followingList,null)
+//                })
+//            }
+//            else{
+//                dbAdapterUser.getFollowersList(docID, EventListener{ followersList, _->
+//                    eventListener.onEvent(followersList,null)
+//                })
+//            }
+//        })
+//    }
+
+    private fun setRecyclerData(followersFollowingList:ArrayList<String>){
+        adapter?.setNewData(followersFollowingList)
+    }
 }
